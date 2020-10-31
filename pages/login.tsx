dc8cf6e099bb1a42477c4fde859fd8eef95a6f28
Anoop <deznit.official@gmail.com>
2020-10-31 18:27:48 +0530

    styling

--- pages/login.tsx
+++ pages/login.tsx
@@ -18,83 +18,80 @@ const login = () => {
       <div className="w-full h-screen flex justify-center align-middle ">
         {/* ========== login section start ========== */}
         <div className="w-full md:w-1/2 flex justify-end  ">
-          <div className=" flex flex-col  my-auto w-full xl:max-w-lg mx-10 xl:mx-32">
-            <div className="w-full">
-              <a href="/">
-                <img src="deznit_logo.svg" alt="logo" />
-              </a>
-              <div className="text-heading text-dark">Signin</div>
+          <div className=" my-auto w-full xl:max-w-lg mx-10 xl:mx-32">
+            <a href="/">
+              <img src="deznit_logo.svg" alt="logo" />
+            </a>
+            <div className="text-heading text-dark ">Signin</div>
 
-              <div className="text-body text-error pt-8">{errorMessage}</div>
-              <div className="text-sm py-2">Email</div>
+            <div className="text-sm py-2 mt-8">Email</div>
+            <Input
+              type="text"
+              placeholder="username@gmail.com"
+              aria-label="Email"
+              className="w-full"
+              variant="outline"
+              height="large"
+              onChange={setEmail}
+            />
+
+            <div className="mt-4">
+              <div className="text-sm py-2">Password</div>
+
               <Input
-                type="text"
-                placeholder="username@gmail.com"
-                aria-label="Email"
+                type="password"
+                placeholder="password"
+                aria-label="Password"
                 className="w-full"
                 variant="outline"
                 height="large"
-                onChange={setEmail}
+                onChange={setPassword}
               />
+            </div>
 
-              <div className="mt-5">
-                <div className="text-sm py-2">Password</div>
+            <div className="text-small text-secondary mt-4">
+              <Link href="/reset">Forgot Password? </Link>
+            </div>
 
-                <Input
-                  type="password"
-                  placeholder="password"
-                  aria-label="Password"
-                  className="w-full"
-                  variant="outline"
-                  height="large"
-                  onChange={setPassword}
+            <Button
+              onClick={handleLogin}
+              disabled={loading}
+              loading={loading}
+              className="my-8 w-full"
+            >
+              Signin
+            </Button>
+            <div className="test-small text-center text-muted">
+              Or signin with
+            </div>
+            <div className="flex flex-row justify-center mt-8">
+              <Button variant="outline" className="w-full mr-2 bg-white">
+                <img
+                  src="icons/google.svg"
+                  alt="google"
+                  width="24px"
+                  height="24px"
+                  className="mr-2"
                 />
-              </div>
-              <div className="mt-6 flex flex-row justify-between">
-                <div className="text-small text-secondary">
-                  <Link href="/reset">Forgot Password? </Link>
-                </div>
-              </div>
-              <Button
-                onClick={handleLogin}
-                disabled={loading}
-                loading={loading}
-                className="my-10 w-full"
-              >
-                Login
+                Google
               </Button>
-              <div className="test-small text-center text-muted">
-                Or signin with
-              </div>
-              <div className="flex flex-row justify-center mt-8">
-                <Button variant="outline" className="w-full mr-2 bg-white">
-                  <img
-                    src="icons/google.svg"
-                    alt="google"
-                    width="24px"
-                    height="24px"
-                    className="mr-2"
-                  />
-                  Google
-                </Button>
-                <Button variant="outline" className="w-full ml-2 ">
-                  <img
-                    src="icons/github.svg"
-                    alt="google"
-                    width="24px"
-                    height="24px"
-                    className="mr-2"
-                  />
-                  Github
-                </Button>
-              </div>
+              <Button variant="outline" className="w-full ml-2 ">
+                <img
+                  src="icons/github.svg"
+                  alt="google"
+                  width="24px"
+                  height="24px"
+                  className="mr-2"
+                />
+                Github
+              </Button>
+            </div>
 
-              <div className="test-small text-center text-muted mt-8">
-                Don&apos;t have an account yet?
-                <span className="text-secondary pl-1">
-                  <Link href="/"> Register for free</Link>
-                </span>
-              </div>
+            <div className="test-small text-center text-muted mt-8">
+              Don&apos;t have an account yet?
+              <span className="text-secondary pl-1">
+                <Link href="/"> Register for free</Link>
+              </span>
             </div>
           </div>
         </div>
